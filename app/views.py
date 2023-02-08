c3070923db7f36d668888069ac3dd6bef29fd29e
Nathaniel Angus <nangus@Nathaniels-MBP.hsd1.pa.comcast.net>
2023-02-07 19:23:05 -0500

    auth complete

--- app/views.py
+++ app/views.py
@@ -1,7 +1,12 @@
 from flask import Blueprint, render_template
+from flask_login import current_user
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def home():
     return render_template('index.html')
+
+@views.route('/search')
+def search():
+    return render_template('search.html', user=current_user)
