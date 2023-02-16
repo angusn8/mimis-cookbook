35bd6d0895a56db45a269c638e5018bc5c6ca791
Nathaniel Angus <nangus@wifi-58-42.nwpd.iup.edu>
2023-02-16 09:54:03 -0500

    started dynamic profile screen implementation. just need to add picture functionality

--- app/models.py
+++ app/models.py
@@ -1,9 +1,25 @@
 from . import db
 from flask_login import UserMixin
 from sqlalchemy.sql import func
+from sqlalchemy.orm import relationship
 
 class User(db.Model, UserMixin):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(150), unique=True)
     password = db.Column(db.String(150))
-    username = db.Column(db.String(150))
+    username = db.Column(db.String(150), unique=True)
+    full_name = db.Column(db.String(150))
+
+    profile_id = db.Column(db.Integer, db.ForeignKey('profile.id'))
+    profile = db.relationship("Profile", backref="profile_id")
+
+class Profile(db.Model):
+    __tablename__ = 'profile'
+    id = db.Column(db.Integer, primary_key=True)
+
+    bio = db.Column(db.String(255), default="")
+    subscribers = db.Column(db.Integer, default=0)
+    rating = db.Column(db.Float, default=0.0)
+    num_reviews = db.Column(db.Integer, default=0)
+
--- app/views.py
+++ app/views.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, render_template
-from flask_login import current_user
+from flask_login import current_user, login_required
+from .models import Profile
 
 views = Blueprint('views', __name__)
 
@@ -8,5 +9,12 @@ def home():
     return render_template('index.html')
 
 @views.route('/search')
+@login_required
 def search():
     return render_template('search.html', user=current_user)
+
+@views.route('/profile')
+@login_required
+def profile():
+    profile = Profile.query.filter_by(id=current_user.id).first()
+    return render_template('profile.html', user=current_user, profile=profile)
