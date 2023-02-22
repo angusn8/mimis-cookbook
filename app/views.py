4509a5555222bde388e546d0445020e784246f55
Nathaniel Angus <nathanielangus0@gmail.com>
2023-02-22 11:18:50 -0500

    Profile update completed

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for
 from flask_login import current_user, login_required
 from .models import Profile
+from . import db
 
 views = Blueprint('views', __name__)
 
@@ -19,8 +20,22 @@ def profile():
     profile = Profile.query.filter_by(id=current_user.id).first()
     return render_template('profile.html', user=current_user, profile=profile)
 
-@views.route('/profile_settings')
+@views.route('/profile/update', methods=['GET','POST'])
 @login_required
-def profile_settings():
+def update_profile():
     profile = Profile.query.filter_by(id=current_user.id).first()
-    return render_template('profile_settings.html', user=current_user, profile=profile)
+    user = current_user
+    full_name = request.form.get('full_name')
+    username = request.form.get('username')
+    email = request.form.get('email')
+    bio = request.form.get('bio')
+
+    if request.method == 'POST':
+        user.full_name = full_name
+        user.username = username
+        user.email = email
+        profile.bio = bio
+
+        db.session.commit()
+        return redirect(url_for('views.profile', user=user, profile=profile))
+    return render_template('profile_settings.html', user=user, profile=profile)
